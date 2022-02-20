bb80b95b4d7f8efd9f61582968ad3199e83581ce
Hakan <74737999+NakxH@users.noreply.github.com>
2022-02-20 22:02:20 +0000

    Added input for new todo task

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
   return (
       <div className='flex justify-center'>
-        <div className='w-1/2 bg-slate-800 text-white text-xl rounded-md m-8 p-4 flex justify-center'>
+        <div className='w-1/2 bg-slate-800 text-white text-xl rounded-md mt-8 mb-2 mx-8 p-4 flex justify-center'>
             <h1>My React ToDo App</h1>
         </div>
       </div>
--- src/Components/TodoForm.js
+++ src/Components/TodoForm.js
@@ -2,11 +2,16 @@ import React from 'react'
 
 export const TodoForm = () => {
   return (
-    <div className='w-1/2 bg-slate-800 text-white text-xl rounded-md m-8 p-4 flex justify-center'>
-      <label>
-        Name: Enter New Todo
-        <input classname='' type='text' name='todo-input'></input>
-      </label>
+    <div className='flex justify-center'>
+      <div className='border-2 border-slate-800	w-1/2 text-xl mb-2 mx-8 rounded-md p-4'>
+        <div className='text-black flex justify-between'>
+        <label>
+          Enter new Todo:<span className='mr-2'></span>
+          <input type='text' name='todo-input'></input>
+        </label>
+          <button type='submit' className='border-2 border-slate-800 rounded-md px-4'>Add</button>
+        </div>
+      </div>
     </div>
   )
 }
